645554b310a45c4ccd98cc27083d41a3f36bb7ed
mannybennett <mannybennett99@gmail.com>
2023-06-12 21:25:21 -0400

    Updates

---
+++ precache-manifest.9dc3569102937be06815a61a29f73d97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ca635b5aa13210d317ee20c8406be8",
+    "url": "/JS411_mapDispatchToProps/index.html"
+  },
+  {
+    "revision": "9a2cdaab14c2b2a777b2",
+    "url": "/JS411_mapDispatchToProps/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "5e42fff3d92f193159ca",
+    "url": "/JS411_mapDispatchToProps/static/js/2.72f72e89.chunk.js"
+  },
+  {
+    "revision": "9a2cdaab14c2b2a777b2",
+    "url": "/JS411_mapDispatchToProps/static/js/main.d0d845f5.chunk.js"
+  },
+  {
+    "revision": "e81d22f9059c0bb53db0",
+    "url": "/JS411_mapDispatchToProps/static/js/runtime~main.2654d7f2.js"
+  }
+]);
