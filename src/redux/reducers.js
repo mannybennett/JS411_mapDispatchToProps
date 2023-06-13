e7e46218e5cffef9733732a17fd32ef50afadd72
mannybennett <mannybennett99@gmail.com>
2023-06-12 21:24:40 -0400

    Complete

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,18 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
 
-const user = (state = null) => state
+const user = (state = null) => state;
 
 // add switch statements in here
 const cars = (state = [], action) => {
-    return state
-}
+    switch (action.type) {
+        case "ADD_CAR":
+            return [...state, action.value]
+        case "REMOVE_CAR":
+            console.log(state)
+            return state.filter((_, idx) => idx !== action.value)
+        default:
+            return state
+    }
+};
 
 export default combineReducers({ user, cars })
